497f6da675668ffcbd35eadc74abeb978df75db9
kamaljeet <kamaljeet8130@gmail.com>
2025-07-22 17:59:26 +0530

    DSA question wip

---
+++ BasicOfJava/src/basic/array/UpperBoundBinarySearch.java
@@ -0,0 +1,27 @@
+package basic.array;
+
+public class UpperBoundBinarySearch {
+
+    public static void main(String[] args) {
+
+    }
+    public static int upperBound(int[] arr , int target){
+
+        return -1;
+    }
+}
+
+
+/*
+Description:
+Given a sorted array, find the index of the first element that is strictly greater than a given target.
+
+This is called an upper bound search.
+
+If no such element exists (all elements ≤ target), return -1 or the size of the array depending on the problem's convention.
+
+arr = [2, 4, 6, 8, 10, 12]
+target = 6
+Index = 3
+Element = 8
+ */
