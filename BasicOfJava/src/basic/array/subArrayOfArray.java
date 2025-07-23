c70dd4de114bfa984c7c0ed4086b1b19790b702d
kamaljeet <kamaljeet8130@gmail.com>
2025-07-23 13:13:28 +0530

    wip

--- BasicOfJava/src/basic/array/subArrayOfArray.java
+++ BasicOfJava/src/basic/array/subArrayOfArray.java
@@ -6,7 +6,7 @@ public class subArrayOfArray {
         subArray(arr);
 
     }
-    public static void subArray(int[] array){
+    public static void subArray(int[] arr){
         for(int i = 0;i<arr.length;i++){
             for (int j = i;j<arr.length;j++){
                 for (int k=i;k<=j;k++){
