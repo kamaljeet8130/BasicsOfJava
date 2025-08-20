142dfa111b4b6a868c7275dd95c8189ddcdf3792
kamaljeet <kamaljeet8130@gmail.com>
2025-08-20 18:57:15 +0530

    wip

--- BasicOfJava/src/Try.java
+++ BasicOfJava/src/Try.java
@@ -1,29 +1,28 @@
-import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Try {
     public static void main(String[] args) {
-        int[] arr ={1,2,2,4,4,4,4,4,4,4,4};
-        for (int i : firstAndLastOccurence(arr,4)){
-            System.out.print(i+",");
-        }
+       int n = 1714636915;
+        System.out.println( arrangeCoins(n));
     }
-    public static int[] firstAndLastOccurence(int[]  nums, int target){
-        int[] arr =new int[2];
-        int first = -1;
-        int last = -1;
-        for(int i = 0;i<nums.length;i++){
-            if(nums[i]==target){
-                if(first==-1){
-                    first = i;
+
+        public static int arrangeCoins(int n) {
+            int start = 1;
+            int end = n;
+            int ans = 0;
+            while(start<=end){
+                int mid =  start  + (end-start)/2;
+                long midStairs = (long)(mid * ( mid+1 ))/2;
+                if(midStairs ==n){
+                    return mid;
                 }
-                last = i;
+                else if(midStairs<n){
+                    ans = mid;
+                    start=mid+1;
+                }
+                else{
+                    end = mid-1;
+                }
             }
-
+            return ans;
         }
-        arr[0] = first;
-        arr[1] = last;
-        return arr;
     }
-
-}
