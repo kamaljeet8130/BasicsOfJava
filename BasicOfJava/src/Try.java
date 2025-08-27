b6747ab55df4044fba5ab1296a85e7e9fd0c1fc2
kamaljeet <kamaljeet8130@gmail.com>
2025-08-27 18:16:47 +0530

    wip

--- BasicOfJava/src/Try.java
+++ BasicOfJava/src/Try.java
@@ -1,28 +1,64 @@
 
 public class Try {
-    public static void main(String[] args) {
-       int n = 1714636915;
-        System.out.println( arrangeCoins(n));
-    }
+        public static void main(String[] args) {
+            int[] bloomDay = {1,10,3,10,2};
+            int m = 3;
+            int k = 2;
+            System.out.println(minDays(bloomDay,m,k));
 
-        public static int arrangeCoins(int n) {
-            int start = 1;
-            int end = n;
-            int ans = 0;
+
+        }
+    public static int minDays(int[] bloomDay, int m, int k) {
+            if((long) m*k>bloomDay.length) return  -1;
+            int start = findMin(bloomDay);
+            int end = findMax(bloomDay);
+            int ans = -1;
             while(start<=end){
-                int mid =  start  + (end-start)/2;
-                long midStairs = (long)(mid * ( mid+1 ))/2;
-                if(midStairs ==n){
-                    return mid;
-                }
-                else if(midStairs<n){
+                int mid = start+(end-start)/2;
+                if(isFlowerBlooming(bloomDay,mid,m,k)){
                     ans = mid;
-                    start=mid+1;
+                    end = mid-1;
                 }
                 else{
-                    end = mid-1;
+                    start= mid+1;
                 }
             }
             return ans;
+    }
+    public static int findMin(int[] arr){
+            int min = Integer.MAX_VALUE;
+            for (int i =0;i<arr.length;i++){
+                if(arr[i]<min){
+                    min = arr[i];
+                }
+            }
+            return min;
+    }
+    public static int findMax(int[] arr){
+        int max = Integer.MIN_VALUE;
+        for (int i =0;i<arr.length;i++){
+            if(arr[i]>max){
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+        public static boolean isFlowerBlooming(int[] arr, int mid , int m ,int k){
+            int counter = 0;
+            int bouquetCounter = 0;
+            for(int i =0;i<arr.length;i++){
+                if(arr[i]<=mid){
+                    counter++;
+                }
+                else if(arr[i]>mid){
+                    counter =0;
+                }
+                if(counter==k){
+                    counter=0;
+                    bouquetCounter++;
+                }
+            }
+            return bouquetCounter >= m;
         }
     }
+
